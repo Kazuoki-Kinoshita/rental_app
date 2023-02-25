ce3eda2738a00ecadb9e3bf0f1516be341857898
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-25 16:43:04 +0900

    デフォルトメッセージの日本語化の一部追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,8 @@
 class Property < ApplicationRecord
   validates :name, presence: true
-  validates :rent, presence: true
+  validates :rent, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :address, presence: true
-  validates :year, presence: true
+  validates :year, presence: true, numericality: { greater_than_or_equal_to: 0 }
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
